1fd07d243fd0cd36e57a1694c75793b72fb14c4b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-19 15:01:55 +0500

    Working on the recycler view

--- app/src/main/java/org/abubaker/affirmations/MainActivity.kt
+++ app/src/main/java/org/abubaker/affirmations/MainActivity.kt
@@ -2,6 +2,8 @@ package org.abubaker.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import org.abubaker.affirmations.adapter.ItemAdapter
 import org.abubaker.affirmations.data.Datasource
 import org.abubaker.affirmations.databinding.ActivityMainBinding
 import javax.sql.DataSource
@@ -20,5 +22,15 @@ class MainActivity : AppCompatActivity() {
 
         // We are going to display total size of the list defined in the Datasource.kt file
         // mBinding.textView.text = Datasource().loadAffirmations().size.toString()
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
